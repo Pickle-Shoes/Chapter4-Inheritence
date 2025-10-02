87579ad3ce0b6d81454c3ec67249872c370b25f2
Luke Martin <luke.martin@trojans.dsu.edu>
2025-10-02 14:59:59 -0500

    Add project files.

---
+++ Chapter4-Inheritence/Animals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Inheritence
+{
+    internal class Animals
+    {
+        //fields/attributes
+        public int NumberOfLegs;
+        public int PairsOfEyes;
+
+        //property
+        public int Age { get; set; }
+
+        //constructor
+           public Animals(int age)
+        {
+            this.Age = age;
+            Console.WriteLine("Animal Created");
+        }
+
+        //Methods
+        public void PrintLegsAndEyes()
+        {
+            Console.WriteLine($"Legs: {this.NumberOfLegs} Eyes: {this.PairsOfEyes * 2}");
+
+        }
+        
+        public void PrintAge()
+        {
+            Console.WriteLine("Age: " + Age);
+        }
+
+    }
+}
---
+++ Chapter4-Inheritence/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Chapter4_Inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals a = new Animals(3);
+            a.PrintAge(); //3
+            a.PrintLegsAndEyes(); //0 0
+            a.PairsOfEyes = 1;
+            a.NumberOfLegs = 4;
+            a.PrintLegsAndEyes(); // 4, 2
+            a.Age = 5;
+            a.PrintAge();
+        }
+    }
+}
