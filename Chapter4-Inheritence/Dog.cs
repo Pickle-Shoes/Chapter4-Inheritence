83766a150868612dac14d3a9184ec0d6cfcfddbd
Luke Martin <luke.martin@trojans.dsu.edu>
2025-10-07 15:21:43 -0500

    Create Dog Class

---
+++ Chapter4-Inheritence/Dog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Inheritence
+{
+    public class Dog : DomesticMammal
+    {
+        //fields/attributes
+        protected override int _numberOfLegs { get { return 4; } }
+        public virtual string Breed { get { return "just a dog."; } }
+        public virtual string BreedFamily { get { return "dog"; } }
+
+
+        //properties
+
+        //constructor
+        public Dog(string name, int age, string favoriteToy) : base(name, age, favoriteToy)
+        {
+            Console.WriteLine("Dog created");
+        }
+
+
+        public Dog(string name, int age, string favoriteToy, bool isPregnant) : base (name, age, favoriteToy, isPregnant)
+        {
+            Console.WriteLine("Dog created");
+        }
+
+
+
+
+        //methods
+        public void PrintBreed()
+        {
+            Console.WriteLine($"Breed: {Breed}");
+        }
+        public void PrintBreedFamily()
+        {
+            Console.WriteLine($"BreedFamily: {BreedFamily}");
+        }
+
+        public override void Talk()
+        {
+            Console.WriteLine($"{Name}: barks");
+        }
+
+    }
+}
--- Chapter4-Inheritence/Program.cs
+++ Chapter4-Inheritence/Program.cs
@@ -5,12 +5,15 @@ namespace Chapter4_Inheritence
     {
         static void Main(string[] args)
         {
-            var a = new DomesticMammal("Rufus", 3, "Ball", true);
+            var a = new Dog("Rufus", 3, "Ball", true);
             a.PrintLegsAndEyes(); // 0, 0
             a.Age = 5;
             a.PrintAge();
             a.Talk();
             Console.WriteLine(a.IsPregnant);
+            a.PrintBreed();
+            a.PrintBreedFamily();
+
         }
     }
 }
