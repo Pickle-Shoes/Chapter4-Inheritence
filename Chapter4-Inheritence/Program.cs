179005dc8b58f2e1ccc9e273fff2ce5e7f0c8ce6
Luke Martin <luke.martin@trojans.dsu.edu>
2025-10-09 15:24:57 -0500

    Used polymorphisim in Main

--- Chapter4-Inheritence/Program.cs
+++ Chapter4-Inheritence/Program.cs
@@ -5,8 +5,9 @@ namespace Chapter4_Inheritence
     {
         static void Main(string[] args)
         {
+            
+            var s = new SmoothFoxTerrier("Rufus", 3, "Ball", true);
             /*
-            var a = new SmoothFoxTerrier("Rufus", 3, "Ball", true);
             a.PrintLegsAndEyes(); // 0, 0
             a.Age = 5;
             a.PrintAge();
@@ -17,12 +18,25 @@ namespace Chapter4_Inheritence
             */
 
             var c = new SiameseCat("Gary", 6, "Yarn", true);
+            /*
             c.PrintLives();
             c.PrintBreed();
             c.PrintLegsAndEyes();
             c.DeductLife();
             c.PrintLives();
             Console.WriteLine(c.IsPregnant);
+            */
+
+            Animals[] animals = { s, c };
+
+            foreach (var a in animals) 
+            {
+                a.PrintLegsAndEyes();
+                a.PrintAge();
+                //a.Talk();
+            }
+
+
         }
     }
 }
